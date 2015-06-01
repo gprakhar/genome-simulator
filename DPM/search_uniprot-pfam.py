101c99c94a982ce55dd5631258ac54574f4425ad
gprakhar <prakhar.aaidu16@gmail.com>
2015-06-01 17:18:19 +0530

    the csv read into string not working as it should, register issue on github

--- DPM/search_uniprot-pfam.py
+++ DPM/search_uniprot-pfam.py
@@ -17,27 +17,22 @@ inputfileName = args.keywordfileName
 #Read input file with csv module and store it as list
 keywordList = list()
 with open(inputfileName) as inputfileHandle:
-	keywordString = csv.reader(inputfileHandle, dialect='excel')
+	keywordString = csv.reader(inputfileHandle)
 	for row in keywordString:
 		keywordList.append(row)
 
-#Query the Uniprot databse with URL encode queries, one by one from the list of keywords
-##URL demo: http://www.uniprot.org/uniprot/?query=gene:wnt AND reviewed:yes AND organism:"Homo sapiens (Human) [9606]"&sort=score&columns=id,entry name,protein names,genes(PREFERRED),organism,length,database(Pfam)
+print len(keywordList)
+for items in keywordList:
+	print "this is a item:%s" % items
 
-#download-->#http://www.uniprot.org/uniprot/?sort=score&desc=&compress=yes&query=gene:wnt%20AND%20reviewed:yes%20AND%20organism:%22Homo%20sapiens%20(Human)%20[9606]%22&fil=&format=fasta&force=yes
+#download-->fasta #http://www.uniprot.org/uniprot/?sort=score&desc=&compress=yes&query=gene:wnt%20AND%20reviewed:yes%20AND%20organism:%22Homo%20sapiens%20(Human)%20[9606]%22&fil=&format=fasta&force=yes
 uniprotDlURL = 'http://www.uniprot.org/uniprot/?sort=score&desc=&compress=yes&query=gene:wnt%20AND%20reviewed:yes%20AND%20organism:%22Homo%20sapiens%20(Human)%20[9606]%22&fil=&format=fasta&force=yes'
 
-#1column-query#download it- gunziped #http://www.uniprot.org/uniprot/?sort=&desc=&compress=yes&query=family:%22wnt%20family%22&fil=organism:%22Homo%20sapiens%20(Human)%20[9606]%22%20AND%20reviewed:yes&format=tab&force=yes&columns=id
+#1column-query- list of Uniprot id's #download it- gunziped #http://www.uniprot.org/uniprot/?sort=&desc=&compress=yes&query=family:%22wnt%20family%22&fil=organism:%22Homo%20sapiens%20(Human)%20[9606]%22%20AND%20reviewed:yes&format=tab&force=yes&columns=id
 uniprotQURL = 'http://www.uniprot.org/uniprot/?sort=&desc=&compress=yes&query=family:%22wnt%20family%22&fil=organism:%22Homo%20sapiens%20(Human)%20[9606]%22%20AND%20reviewed:yes&format=tab&force=yes&columns=id'
-#uniprotPayload = {'gene': 'wnt', 'reviewed': 'yes', 'organism': '"Homo sapiens (Human) [9606]"', 'sort': 'score', 'columns[]': ['id', 'entry name', 'protein names', 'genes(PREFERRED)', 'organism', 'length', 'database(Pfam)']}
 
-#r = requests.get(uniprotQURL)
-#print r.encoding
-#print r.text
-
-
-
-##to Download a fasta or similar file
+##to Download a fasta or similar file or 
+#for 
 r = requests.get(uniprotQURL, stream=True)
 if r.status_code == 200:
 	print r.status_code
