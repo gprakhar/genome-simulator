e309416af664c402cc9ce2e6dc680d38c4ebaed0
gprakhar <prakhar.aaidu16@gmail.com>
2015-06-09 05:42:15 +0530

    variable in URL not translating

--- DPM/search_uniprot-pfam.py
+++ DPM/search_uniprot-pfam.py
@@ -1,7 +1,7 @@
 #Script to query web pages at Uniprot and Pfam and download the relevant data
 #No proxy handling
 #author: prakhar gaur
-#date: Fri Oct 10 10:36:35 IST 2014
+#date: Thu May 28 IST 2015
 
 import argparse
 import csv
@@ -15,24 +15,24 @@ args = parser.parse_args()
 inputfileName = args.keywordfileName
 
 #Read input file with csv module and store it as list
+#Line change means new entry
 keywordList = list()
 with open(inputfileName) as inputfileHandle:
 	keywordString = csv.reader(inputfileHandle)
 	for row in keywordString:
 		keywordList.append(row)
 
-print len(keywordList)
-for items in keywordList:
-	print "this is a item:%s" % items
-
-#download-->fasta #http://www.uniprot.org/uniprot/?sort=score&desc=&compress=yes&query=gene:wnt%20AND%20reviewed:yes%20AND%20organism:%22Homo%20sapiens%20(Human)%20[9606]%22&fil=&format=fasta&force=yes
-uniprotDlURL = 'http://www.uniprot.org/uniprot/?sort=score&desc=&compress=yes&query=gene:wnt%20AND%20reviewed:yes%20AND%20organism:%22Homo%20sapiens%20(Human)%20[9606]%22&fil=&format=fasta&force=yes'
+uniprotURL-fasta = 'http://www.uniprot.org/uniprot/?sort=score&desc=&compress=yes&query=gene:wnt%20AND%20reviewed:yes%20AND%20organism:%22Homo%20sapiens%20(Human)%20[9606]%22&fil=&format=fasta&force=yes'
 
 #1column-query- list of Uniprot id's #download it- gunziped #http://www.uniprot.org/uniprot/?sort=&desc=&compress=yes&query=family:%22wnt%20family%22&fil=organism:%22Homo%20sapiens%20(Human)%20[9606]%22%20AND%20reviewed:yes&format=tab&force=yes&columns=id
-uniprotQURL = 'http://www.uniprot.org/uniprot/?sort=&desc=&compress=yes&query=family:%22wnt%20family%22&fil=organism:%22Homo%20sapiens%20(Human)%20[9606]%22%20AND%20reviewed:yes&format=tab&force=yes&columns=id'
+uniprotQURL = 'http://www.uniprot.org/uniprot/?sort=&desc=&compress=yes&query=family:wnt&fil=organism:%22Homo%20sapiens%20(Human)%20[9606]%22%20AND%20reviewed:yes&format=tab&force=yes&columns=id'
+
+for keyword in keywordList:
+	uniprotQURL = 'http://www.uniprot.org/uniprot/?sort=&desc=&compress=yes&query=family:%d&fil=organism:%22Homo%20sapiens%20(Human)%20[9606]%22%20AND%20reviewed:yes&format=tab&force=yes&columns=id' % keyword
+	print uniprotQURL
 
 ##to Download a fasta or similar file or 
-#for 
+##
 r = requests.get(uniprotQURL, stream=True)
 if r.status_code == 200:
 	print r.status_code
@@ -40,3 +40,4 @@ if r.status_code == 200:
 	        r.raw.decode_content = True
         	shutil.copyfileobj(r.raw, fileHandle) 
 
+	
