e1d9a7b2681cf21799774238c6b88b793f513fe8
gprakhar <prakhar.aaidu16@gmail.com>
2015-07-08 00:35:19 +0530

    kinship code minor adition

--- kinship/prototype-ver1.py
+++ kinship/prototype-ver1.py
@@ -6,6 +6,15 @@ import argparse
 
 #parse arguments and create usage text
 parser = argparse.ArgumentParser()
-parser.add_argument('keywordfileName', metavar='filename', help='File with comma seprated list of keywords that have to be searched in Uniprot database')
+parser.add_argument('numberofgenerations', metavar='numGen', default='10', type=int, help='Number of progeny generations that have to be simulated')
+parser.add_argument('numberofalleles', metavar='numAlleles', default='1', type=int, help='Number of Alleles for simulatiom')
+parser.add_argument('numberofchromosomes', metavar='numChr', default='1', type=int, help='Number of Chromosomes')
+
 args = parser.parse_args()
-inputfileName = args.keywordfileName
+numGen = args.numberofgenerations
+numAlleles = args.numberofalleles
+numChr = args.numberofchromosomes
+
+print "Gen=%d Alleles=%d Chr=%d" % (numGen, numAlleles, numChr)
+
+
