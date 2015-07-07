1b5dbd87e5f2cb852b9f9b7035abb6646c1b48a0
gprakhar <prakhar.aaidu16@gmail.com>
2015-07-07 20:03:28 +0530

    genome simulator code

--- DPM/search_uniprot-pfam.py
+++ DPM/search_uniprot-pfam.py
@@ -34,24 +34,13 @@ with open(inputfileName) as inputfileHandle:
 	for row in keywordString:
 		keywordList.append(row)
 
-#uniprotURL-fasta = 'http://www.uniprot.org/uniprot/?sort=score&desc=&compress=yes&query=gene:wnt%20AND%20reviewed:yes%20AND%20organism:%22Homo%20sapiens%20(Human)%20[9606]%22&fil=&format=fasta&force=yes'
-
-#1column-query- list of Uniprot id's #download it- gunziped #http://www.uniprot.org/uniprot/?sort=&desc=&compress=yes&query=family:%22wnt%20family%22&fil=organism:%22Homo%20sapiens%20(Human)%20[9606]%22%20AND%20reviewed:yes&format=tab&force=yes&columns=id
-#uniprotQURL = 'http://www.uniprot.org/uniprot/?sort=&desc=&compress=yes&query=family:wnt&fil=organism:%22Homo%20sapiens%20(Human)%20[9606]%22%20AND%20reviewed:yes&format=tab&force=yes&columns=id'
 uniprotFastaDl = 'http://www.uniprot.org/uniprot/Q14623.fasta'
-'''
-for keyword in keywordList:
-#	uniprotQURL = 'http://www.uniprot.org/uniprot/?sort=&desc=&compress=yes&query=family:%d&fil=organism:%22Homo%20sapiens%20(Human)%20[9606]%22%20AND%20reviewed:yes&format=tab&force=yes&columns=id' % keyword
-	uniprotFastaDl = 'http://www.uniprot.org/uniprot/%s.fasta' % str(keyword)
-	print uniprotFastaDl
-'''
-##to Download a fasta or similar file or 
-##
 
+##to Download a fasta or similar file or 
 for keyword in keywordList:
 	uniprotID = ', '.join(map(str, keyword))
 	uniprotFastaDl = 'http://www.uniprot.org/uniprot/%s.fasta' % uniprotID
-	r = requests.get(uniprotFastaDl, stream=True, proxies=proxyDict)
+	r = requests.get(uniprotFastaDl, stream=True)#, proxies=proxyDict)
 	if r.status_code == 200:
 		print r.status_code
 		with open("%s.fa" % uniprotID, 'wb') as fileHandle:
